a95822d8e785c44daef7d03ec1063f4d9ac28c3d
Hasham664 <m.hasham3222@gmail.com>
2025-08-21 17:47:05 +0500

    push

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
   origin: [
-    'http://localhost:5173', // for local development
-    
+    'https://instaclonerepo.onrender.com',
+    'http://localhost:5173',
+    // for local development
   ],
   credentials: true,
 };
